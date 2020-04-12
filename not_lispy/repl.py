a8e769cf905804b830c9872b6ff4e3b90c9dce39
Antti Juvonen <akajuvonen@users.noreply.github.com>
2020-04-12 14:23:41 +0900

    FIX: read first, evaluate second

--- not_lispy/repl.py
+++ not_lispy/repl.py
@@ -3,7 +3,7 @@ from not_lispy.lisp import read, evaluate
 
 def main():
     while (expression := input('not-lispy>')) != '(exit)':
-        print(read(evaluate(expression)))
+        print(evaluate(read(expression)))
 
 
 if __name__ == '__main__':
