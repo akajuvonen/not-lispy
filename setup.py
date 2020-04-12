2f300843ed7d26a22923c832e891c745ab2ce9e5
Antti Juvonen <akajuvonen@users.noreply.github.com>
2020-04-12 14:39:31 +0900

    Add repl as an entrypoint

--- setup.py
+++ setup.py
@@ -3,9 +3,13 @@ from setuptools import setup, find_packages
 
 setup(
     name='not-lispy',
-    version='0.2.0',
+    version='0.3.0',
     description='A Lisp interpreter in Python',
     author='Antti Juvonen',
     packages=find_packages(),
     python_requires='>=3.8',
-    install_requires=['click', 'attrs'])
+    install_requires=['click', 'attrs'],
+    entry_points='''
+    [console_scripts]
+    notlispy-repl=not_lispy:repl
+    ''')
