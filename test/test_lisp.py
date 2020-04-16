893b1d46f8df2264721de36843dbbb809758c614
Antti Juvonen <akajuvonen@users.noreply.github.com>
2020-04-16 21:29:15 +0900

    TEST: end-to-end test updated

--- test/test_lisp.py
+++ test/test_lisp.py
@@ -2,10 +2,6 @@ import pytest
 from not_lispy import read, evaluate
 
 
-def test_tokenize():
-    assert read('(+ 1 (- 2 3))') == ['+', 1, ['-', 2, 3]]
-
-
-def test_evaluate():
-    assert evaluate(['+', 1, ['*', 2, 3]]) == 7
-    assert evaluate(['/', ['-', 10, 4], 2]) == 3
+@pytest.mark.parametrize('program, expected', [('(+ 1 2)', 3), ('(- (/ 10 2) 3)', 2), ('(* 2 2 3)', 12)])
+def test_read_eval(program, expected):
+    assert evaluate(read(program)) == expected
