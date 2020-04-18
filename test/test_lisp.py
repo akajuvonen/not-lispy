23d51a29cd2a87ed0309cfd3cbd506571ede4f7a
Antti Juvonen <akajuvonen@users.noreply.github.com>
2020-04-18 15:02:40 +0900

    TEST: add lambda procedure to test

--- test/test_lisp.py
+++ test/test_lisp.py
@@ -2,6 +2,9 @@ import pytest
 from not_lispy import read, evaluate
 
 
-@pytest.mark.parametrize('program, expected', [('(+ 1 2)', 3), ('(- (/ 10 2) 3)', 2), ('(* 2 2 3)', 12)])
+@pytest.mark.parametrize('program, expected', [('(+ 1 2)', 3),
+                                               ('(- (/ 10 2) 3)', 2),
+                                               ('(* 2 2 3)', 12),
+                                              ('((lambda (x y) (+ x y)) 1 2)', 3)])
 def test_read_eval(program, expected):
     assert evaluate(read(program)) == expected
