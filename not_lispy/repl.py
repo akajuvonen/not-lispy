a9eceef7c831c49ba9a6e08a4b1e230b2e9887a7
Antti Juvonen <akajuvonen@users.noreply.github.com>
2020-04-19 16:02:09 +0900

    Don't print None in repl

--- not_lispy/repl.py
+++ not_lispy/repl.py
@@ -7,4 +7,6 @@ readline.parse_and_bind('set editing-mode vi')
 
 def repl():
     while (expression := input('not-lispy>')) != '(exit)':
-        print(evaluate(read(expression)))
+        result = evaluate(read(expression))
+        if result is not None:
+            print(result)
