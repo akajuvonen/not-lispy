d8425ea74d796dadf9cab791706ae772f0338d4c
Antti Juvonen <akajuvonen@users.noreply.github.com>
2020-04-30 21:26:15 +0900

    Update version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='not-lispy',
-    version='0.6.0',
+    version='0.8.0',
     description='A Lisp interpreter in Python',
     author='Antti Juvonen',
     packages=find_packages(),
